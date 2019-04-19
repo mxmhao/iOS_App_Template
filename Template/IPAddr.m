36c4bfc845dde5fa525ebf1bc4c4a1656b730a40
mxmhao <mxmhao@163.com>
2019-04-19 17:45:21 +0800

    添加蜂窝网络获取模板和IPv6获取模板

--- Template/IPAddr.m
+++ Template/IPAddr.m
@@ -1,22 +1,48 @@
 #include <arpa/inet.h>
 #include <ifaddrs.h>
-+ (NSString *)IPAddress
+
+//"utun0": VPN
+
++ (NSString *)IPAddressForWiFi
 {
     struct ifaddrs *interfaces = NULL;
     if (getifaddrs(&interfaces) != 0) return nil;
     NSString *address = nil;
     
-    struct ifaddrs *addr = NULL;
-    addr = interfaces;
-    while (addr != NULL) {
+    for (struct ifaddrs *addr = interfaces; addr != NULL; addr = addr->ifa_next) {
         //tvOS：en0：网线网卡；en1：WiFi网卡
-        //iOS：en1：WiFi网卡
+        //iOS：en0：WiFi网卡
         if(addr->ifa_addr->sa_family == AF_INET &&
            (strcmp("en0", addr->ifa_name) == 0 || strcmp("en1", addr->ifa_name) == 0)) {
             address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)addr->ifa_addr)->sin_addr)];
             break;
         }
-        addr = addr->ifa_next;
+//        if (addr->ifa_addr->sa_family == AF_INET6) {//IPv6
+//            char ip6[INET6_ADDRSTRLEN];
+//            if(inet_ntop(AF_INET6, &((struct sockaddr_in6 *)addr->ifa_addr)->sin6_addr, ip6, INET6_ADDRSTRLEN)) {
+//                address = [NSString stringWithUTF8String:ip6];
+//            }
+//        }
+    }
+    // Free memory
+    freeifaddrs(interfaces);
+    
+    return address;
+}
+
+//蜂窝网络地址
++ (NSString *)IPAddressForWWAN
+{
+    struct ifaddrs *interfaces = NULL;
+    if (getifaddrs(&interfaces) != 0) return nil;
+    NSString *address = nil;
+    
+    for (struct ifaddrs *addr = interfaces; addr != NULL; addr = addr->ifa_next) {
+        if(addr->ifa_addr->sa_family == AF_INET &&
+           strcmp("pdp_ip0", addr->ifa_name) == 0 {
+            address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)addr->ifa_addr)->sin_addr)];
+            break;
+        }
     }
     // Free memory
     freeifaddrs(interfaces);
